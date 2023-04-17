49f598a40f6e872c4763dc05f3198ab7da350fe2
Sayat <66158646+sayatumarov@users.noreply.github.com>
2023-04-17 16:46:38 +0600

    part of the rest api with postgres

--- main.go
+++ main.go
@@ -1,66 +1,87 @@
 package main
 
 import (
-	"errors"
-	"github.com/gin-gonic/gin"
+	"api/models"
+	"api/storage"
+	"github.com/gofiber/fiber"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+	"log"
 	"net/http"
+	"os"
 )
 
-type todo struct {
-	ID        string `json:"id"`
-	Item      string `json:"item"`
-	Completed bool   `json:"completed"`
+type Book struct {
+	Author    string `json:"author"`
+	Title     string `json:"title"`
+	Publisher string `json:"publisher"`
+}
+type Repository struct {
+	DB *gorm.DB
 }
 
-var todos = []todo{
-	{"1", "Homework", true},
-	{"2", "Task", false},
-	{"3", "Cleaning", true},
-}
+func (r *Repository) CreateBook(context *fiber.Ctx) error {
+	book := Book{}
+	err := context.BodyParser(&book)
+	if err != nil {
+		context.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"message": "request failed"})
+		return err
+	}
+	err = r.DB.Create(&book).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not create a book"})
+		return err
+	}
+	context.Status(http.StatusOK).JSON(
+		&fiber.Map{"message": "everything is ok"})
 
-func GetTodos(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, todos)
+	return nil
 }
-func GetTodo(context *gin.Context) {
-	id := context.Param("id")
-	todo, err := GetTodoById(id)
-	if err == nil {
-		context.IndentedJSON(http.StatusOK, todo)
-	} else {
-		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
-	}
-}
-func ToggleTodoStatus(context *gin.Context) {
-	id := context.Param("id")
-	todo, err := GetTodoById(id)
+func (r *Repository) GetBooks(context *fiber.Ctx) error {
+	bookModels := &[]models.Books{}
+	err := r.DB.Find(bookModels).Error
 	if err != nil {
-		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not get books",
+				"data": bookModels,
+			})
+		return err
 	}
-	todo.Completed = !todo.Completed
-	context.IndentedJSON(http.StatusOK, todo)
+	context.Status(http.StatusOK).JSON(
+		&fiber.Map{"message": "everything is ok"})
+	return nil
 }
-func GetTodoById(id string) (*todo, error) {
-	for i, t := range todos {
-		if t.ID == id {
-			return &todos[i], nil
-		}
-	}
-	return nil, errors.New("there's no data with that id")
-}
-func AddTodo(context *gin.Context) {
-	var newTodo todo
-	err := context.Bind(&newTodo)
-	if err != nil {
-		return
-	}
-	todos = append(todos, newTodo)
-	context.IndentedJSON(http.StatusCreated, newTodo)
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create_book", r.CreateBook)
+	api.Delete("/delete_book", r.DeleteBook)
+	api.Get("/get_book/:id", r.GetBookByID)
+	api.Get("/books", r.GetBooks)
 }
 func main() {
-	router := gin.Default()
-	router.GET("/todos", GetTodos)
-	router.GET("/todos/:id", GetTodo)
-	router.PATCH("/todos/:id", ToggleTodoStatus)
-	router.POST("/add", AddTodo)
-	router.Run("localhost:9090")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := &storage.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASS"),
+		User:     os.Getenv("DB_USER"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+		DBName:   os.Getenv("DB_NAME"),
+	}
+	db, err := storage.NewConnection(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := Repository{
+		DB: db,
+	}
+	app := fiber.New()
+	r.SetupRoutes(app)
+	app.Listen(":8080")
+
 }
